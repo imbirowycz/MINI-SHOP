e405c07f24d282dacac5f0ff5375dd3882224dce
Andrzej Imbirowycz <a.imbirowycz@gmail.com>
2017-09-11 17:59:10 +0200

    work in progress

---
+++ src/main/java/pl/spring/config/WebSecurityConfig.java
@@ -0,0 +1,8 @@
+package pl.spring.config;
+
+/**
+ * @Copyright 11.09.17
+ * @autor Andrzej I.
+ */
+public class WebSecurityConfig {
+}
--- src/main/java/pl/spring/controller/Hello.java
+++ src/main/java/pl/spring/controller/Hello.java
@@ -1,11 +1,11 @@
 package pl.spring.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class Hello {
-    @RequestMapping("/")
+    @GetMapping("/")
     public String hello() {
         return "index";
     }
